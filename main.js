57dd46d946850d1496b4062db72a5c6e3644e3c1
SethFeingold <sfeingold1104@gmail.com>
2019-10-13 11:57:42 -0700

    key variable created so changes to on screen buttons can be applied

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 window.addEventListener('keydown', (e) => {
     const datakey = e.keyCode;
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.textContent = 'ME';
 })
